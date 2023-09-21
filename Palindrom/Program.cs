f8157eee1e2021912c054b2b78ea63d445a711b8
Irinapreo <irinapreobrazhenskaya100@gmail.com>
2023-09-21 18:46:36 +0200

    initiate commit

---
+++ Palindrom/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine("Enter a word to check if it's a palindrome"); 
+string word = Console.ReadLine(); 
+string palindromeCheck = word; 
+
+for (int i = 0; i < word.Length; i++) 
+{ 
+    var letter = word[i]; 
+    string index = letter.ToString(); 
+    word = word.Remove(i, 1).Insert(0, index); 
+}
+
+for (int i = 0; i < palindromeCheck.Length; i++) 
+{ 
+    var letter = word[i]; 
+    string index = letter.ToString(); 
+    palindromeCheck = palindromeCheck.Remove(i, 1).Insert(0, index); 
+}
+
+if (palindromeCheck == word) 
+{ 
+    Console.WriteLine("The word is a palindrome"); 
+} 
+else 
+{ 
+    Console.WriteLine("The word is not a palindrome"); 
+}
